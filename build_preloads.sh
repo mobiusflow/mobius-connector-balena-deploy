03412a426e23f9655923c9f8a96229a52644a76a
EJSRobinson <elliotjs.robinson@gmail.com>
2025-01-27 11:56:39 +0000

    Bump to v1.1.0

--- build_preloads.sh
+++ build_preloads.sh
@@ -11,11 +11,11 @@ balena login --token $LOCAL_BALENA_TOKEN
 # RPI4
 rm mf_connectors_rpi4_preload_*
 cp ./preload/rpi4.img ./preload/mf_connectors_rpi4_preload_${buildver}_${connectorver}.img
-balena preload ./preload/mf_connectors_rpi4_preload_${buildver}_${connectorver}.img --fleet chris88/mobiusflow_connectors_rpi4
+balena preload ./preload/mf_connectors_rpi4_preload_${buildver}_${connectorver}.img --fleet chris88/mobiusflow-connector-rpi4
 echo 'Outfile: ./preload/mf_connectors_rpi4_preload_'${buildver}'_'${connectorver}'.img'
 
 # RPI5
-rm mf_connectors_rpi5_preload_*
-cp ./preload/rpi5.img ./preload/mf_connectors_rpi5_preload_${buildver}_${connectorver}.img
-balena preload  ./preload/mf_connectors_rpi5_preload_${buildver}_${connectorver}.img --fleet chris88/mobiusflow_connectors_rpi5
-echo 'Outfile: ./preload/mf_connectors_rpi5_preload_'${buildver}'_'${connectorver}'.img'
+# rm mf_connectors_rpi5_preload_*
+# cp ./preload/rpi5.img ./preload/mf_connectors_rpi5_preload_${buildver}_${connectorver}.img
+# balena preload  ./preload/mf_connectors_rpi5_preload_${buildver}_${connectorver}.img --fleet chris88/mobiusflow-connector-rpi5
+# echo 'Outfile: ./preload/mf_connectors_rpi5_preload_'${buildver}'_'${connectorver}'.img'
--- publish_dev.sh
+++ publish_dev.sh
@@ -4,6 +4,16 @@
 
 balena login --token $LOCAL_BALENA_TOKEN
 
+# Our RPI4s
+cd ./architectures/rpi4
+balena push chris88/mobiusflow_connectors_rpi4
+cd ../../
+
+# Our RPI5s
+cd ./architectures/rpi5
+balena push chris88/mobiusflow_connectors_rpi5
+cd ../../
+
 # RPI4
 cd ./architectures/rpi4
 balena push chris88/mobiusflow-connector-rpi4
