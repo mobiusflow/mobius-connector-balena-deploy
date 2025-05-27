7fd63c1e1b35e73520bde68a55144ad2eb4b47a3
EJSRobinson <elliotjs.robinson@gmail.com>
2025-05-27 11:59:39 +0100

    Bump to 1.2.0

--- publish_dev.sh
+++ publish_dev.sh
@@ -4,22 +4,27 @@
 
 balena login --token $LOCAL_BALENA_TOKEN
 
+# Dev RPI4s
+# cd ./architectures/rpi4
+# balena push chris88/mf_test_connectors_rpi4
+# cd ../../
+
 # Our RPI4s
 cd ./architectures/rpi4
 balena push chris88/mobiusflow_connectors_rpi4
 cd ../../
 
-# Our RPI5s
+# # Our RPI5s
 cd ./architectures/rpi5
 balena push chris88/mobiusflow_connectors_rpi5
 cd ../../
 
-# RPI4
-cd ./architectures/rpi4
-balena push chris88/mobiusflow-connector-rpi4
-cd ../../
+# # RPI4
+# cd ./architectures/rpi4
+# balena push chris88/mobiusflow-connector-rpi4
+# cd ../../
 
-# RPI5
-cd ./architectures/rpi5
-balena push chris88/mobiusflow-connector-rpi5
-cd ../../
+# # RPI5
+# cd ./architectures/rpi5
+# balena push chris88/mobiusflow-connector-rpi5
+# cd ../../
